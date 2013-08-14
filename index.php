08cac10fd75197b69f69fa6304ca6b29df86e6d4
catzilla-007 <jewaysongonzalgo@gmail.com>
2013-08-14 21:07:55 +0800

    initialized repository

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/* index.php
+	created by: Jewayson Gonzalgo
+	created on: Aug 14, 2013
+	last edited: Aug 14, 2013
+	desc: Operating System Project
+
+	Shortest Job First Preemptive sample
+	
+*/
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>OS SJF-P</title>
+		<link rel="stylesheet" type="text/css" href="css/ui.css" />
+	</head>
+	<body>
+		<div class="title">
+			<h1>Operating System Project<br/><small>Shortest Job First - Preemptive</small></h1>
+		</div>
+	</body>
+</html>
